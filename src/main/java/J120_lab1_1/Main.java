d13e853486750051a1ccd8f45283edf8e7d740ba
Роман Казаченко <kazachenkori@bk.ru>
2023-11-25 14:41:56 +0300

    Final

---
+++ src/main/java/J120_lab1_1/Main.java
@@ -0,0 +1,28 @@
+package J120_lab1_1;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+       TelephoneNumber tel1 = new TelephoneNumber("990", "1028983");
+       TelephoneNumber tel2 = new TelephoneNumber("1909", "562323");
+
+        System.out.println("-------Реализация Map ассоциативного массива------------");
+        Map<TelephoneNumber, String> contacts = new HashMap<>();
+        contacts.put(tel1, "Oleg");
+        contacts.put(tel2, "Semen");
+        contacts.put(tel1,"Vasiliy");
+        contacts.forEach((key, value) -> System.out.println("Имя: " + value + " Телефон: " + key));
+
+        System.out.println("-------Реализация Set множества------------");
+        Set <TelephoneNumber> contactsSet= new HashSet<>();
+        contactsSet.add(tel1);
+        contactsSet.add(tel2);
+        contactsSet.add(tel1);
+        contactsSet.forEach( telephoneNumber -> System.out.println("Телефон: " + telephoneNumber));
+    }
+}
---
+++ src/main/java/J120_lab1_1/TelephoneNumber.java
@@ -0,0 +1,54 @@
+package J120_lab1_1;
+
+import java.util.Objects;
+
+public class TelephoneNumber {
+
+    private String regionCode;
+    private String localNumber;
+
+    public void setRegionCode(String regionCode) {
+        if (regionCode.matches("-?\\d+") && regionCode.length()>=3 && regionCode.length() <=4){
+            this.regionCode = regionCode;
+        } else {
+            throw new IllegalArgumentException ("Неверный формат кода региона");
+        }
+    }
+
+    public void setLocalNumber(String localNumber) {
+        if (localNumber.matches("-?\\d+") && localNumber.length() > 5 && localNumber.length() < 8 ) {
+            this.localNumber = localNumber;
+        }else {
+            throw new IllegalArgumentException ("Неверный формат номера");
+        }
+    }
+
+    public TelephoneNumber(String regionCode, String localNumber) {
+        setRegionCode(regionCode);
+        setLocalNumber(localNumber);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        int numberLength = localNumber.length();
+        sb.append("(").append(regionCode).append(")");
+        sb.append(localNumber.substring(0, (numberLength - 4))).append("-")
+                .append(localNumber.substring((numberLength - 4), (numberLength - 2))).append("-")
+                .append(localNumber.substring((numberLength - 2), numberLength));
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TelephoneNumber that = (TelephoneNumber) o;
+        return regionCode.equals(that.regionCode) && localNumber.equals(that.localNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(regionCode, localNumber);
+    }
+}
